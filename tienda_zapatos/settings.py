d50675ca67982f3f9197be7c55268876c6d9da69
christ <ch.louidor@duocuc.cl>
2024-06-28 21:07:59 -0400

    fatla crud

--- tienda_zapatos/settings.py
+++ tienda_zapatos/settings.py
@@ -127,3 +127,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL='inicio'
 LOGOUT_REDIRECT_URL='inico'
+
+STATIC_URL='static/'
+STATICFILES_DIRS = [BASE_DIR/ 'static']
+
+MEDIA_URL='media/'
+NEDIA_ROOT = BASE_DIR/ 'media'
